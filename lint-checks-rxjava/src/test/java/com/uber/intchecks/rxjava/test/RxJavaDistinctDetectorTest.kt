dcb804094f3dbb60953997d31c2acc83595a4a44
Shaishav Gandhi <shaishav@uber.com>
2019-09-30 20:53:12 -0700

    Spotless

--- lint-checks-rxjava/src/test/java/com/uber/intchecks/rxjava/test/RxJavaDistinctDetectorTest.kt
+++ lint-checks-rxjava/src/test/java/com/uber/intchecks/rxjava/test/RxJavaDistinctDetectorTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2019. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.uber.intchecks.rxjava.test
 
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
@@ -5,16 +20,16 @@ import com.uber.lintchecks.base.test.LintTestBase
 import com.uber.lintchecks.rxjava.RxJavaDistinctDetector
 import org.junit.Test
 
-class RxJavaDistinctDetectorTest: LintTestBase() {
+class RxJavaDistinctDetectorTest : LintTestBase() {
 
   @Test
   fun testDistinct_kotlin_observable_fails() {
     lint()
         .files(rxjava2(), kotlinSource("""
           package foo
-          
+
           import io.reactivex.Observable
-          
+
           class Activity {
             fun something() {
               Observable.just(1)
@@ -34,9 +49,9 @@ class RxJavaDistinctDetectorTest: LintTestBase() {
     lint()
         .files(rxjava2(), javaSource("""
           package foo;
-          
+
           import io.reactivex.Observable;
-          
+
           class Activity {
             void something() {
               Observable.just(1)
@@ -56,9 +71,9 @@ class RxJavaDistinctDetectorTest: LintTestBase() {
     lint()
         .files(rxjava2(), kotlinSource("""
           package foo
-          
+
           import io.reactivex.Flowable
-          
+
           class Activity {
             fun something() {
               Flowable.just(1)
@@ -78,9 +93,9 @@ class RxJavaDistinctDetectorTest: LintTestBase() {
     lint()
         .files(rxjava2(), javaSource("""
           package foo;
-          
+
           import io.reactivex.Flowable;
-          
+
           class Activity {
             void something() {
               Flowable.just(1)
@@ -100,9 +115,9 @@ class RxJavaDistinctDetectorTest: LintTestBase() {
     lint()
         .files(rxjava2(), kotlinSource("""
           package foo
-          
+
           import io.reactivex.Observable
-          
+
           class Activity {
             fun something() {
               Observable.just(1)
