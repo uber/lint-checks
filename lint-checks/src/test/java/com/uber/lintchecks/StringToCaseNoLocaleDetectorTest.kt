a56893fe8e22754052f4be61b08bccff466a6326
Shaishav Gandhi <shaishav@uber.com>
2019-10-10 20:26:56 -0700

    Add test

--- lint-checks/src/test/java/com/uber/lintchecks/StringToCaseNoLocaleDetectorTest.kt
+++ lint-checks/src/test/java/com/uber/lintchecks/StringToCaseNoLocaleDetectorTest.kt
@@ -4,7 +4,8 @@ import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
 import com.uber.lintchecks.base.test.LintTestBase
 import org.junit.Test
 
-class StringToCaseNoLocaleDetector: LintTestBase() {
+class StringToCaseNoLocaleDetectorTest: LintTestBase() {
+
   @Test
   fun kotlinWithoutLocale_errors() {
     lint()
@@ -17,5 +18,65 @@ class StringToCaseNoLocaleDetector: LintTestBase() {
             }
           }
         """))
+        .detector(StringToCaseNoLocaleDetector())
+        .issues(StringToCaseNoLocaleDetector.ISSUE)
+        .run()
+        .expectErrorCount(1)
+  }
+
+  @Test
+  fun kotlinWithLocale_clean() {
+    lint()
+        .files(kotlinSource("""
+          package foo
+          import java.util.Locale
+          
+          class Sample {
+            fun sample() {
+              "hello".toUpperCase(Locale.CANADA)
+            }
+          }
+        """))
+        .detector(StringToCaseNoLocaleDetector())
+        .issues(StringToCaseNoLocaleDetector.ISSUE)
+        .run()
+        .expectClean()
+  }
+
+  @Test
+  fun javaWithoutLocale_errors() {
+    lint()
+        .files(javaSource("""
+          package foo;
+          
+          class Sample {
+            void sample() {
+              "hello".toUpperCase();
+            }
+          }
+        """))
+        .detector(StringToCaseNoLocaleDetector())
+        .issues(StringToCaseNoLocaleDetector.ISSUE)
+        .run()
+        .expectErrorCount(1)
+  }
+
+  @Test
+  fun javaWithLocale_clean() {
+    lint()
+        .files(javaSource("""
+          package foo;
+          import java.util.Locale;
+          
+          class Sample {
+            fun sample() {
+              "hello".toLowerCase(Locale.CANADA);
+            }
+          }
+        """))
+        .detector(StringToCaseNoLocaleDetector())
+        .issues(StringToCaseNoLocaleDetector.ISSUE)
+        .run()
+        .expectClean()
   }
 }
