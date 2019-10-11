de83cd9f38d58a129f9bd0f81b5fb88391681cee
Shaishav Gandhi <shaishav@uber.com>
2019-10-10 20:27:12 -0700

    Spotless

--- lint-checks/src/test/java/com/uber/lintchecks/StringToCaseNoLocaleDetectorTest.kt
+++ lint-checks/src/test/java/com/uber/lintchecks/StringToCaseNoLocaleDetectorTest.kt
@@ -1,17 +1,32 @@
+/*
+ * Copyright (C) 2019. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.uber.lintchecks
 
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
 import com.uber.lintchecks.base.test.LintTestBase
 import org.junit.Test
 
-class StringToCaseNoLocaleDetectorTest: LintTestBase() {
+class StringToCaseNoLocaleDetectorTest : LintTestBase() {
 
   @Test
   fun kotlinWithoutLocale_errors() {
     lint()
         .files(kotlinSource("""
           package foo
-          
+
           class Sample {
             fun sample() {
               "hello".toUpperCase()
@@ -30,7 +45,7 @@ class StringToCaseNoLocaleDetectorTest: LintTestBase() {
         .files(kotlinSource("""
           package foo
           import java.util.Locale
-          
+
           class Sample {
             fun sample() {
               "hello".toUpperCase(Locale.CANADA)
@@ -48,7 +63,7 @@ class StringToCaseNoLocaleDetectorTest: LintTestBase() {
     lint()
         .files(javaSource("""
           package foo;
-          
+
           class Sample {
             void sample() {
               "hello".toUpperCase();
@@ -67,7 +82,7 @@ class StringToCaseNoLocaleDetectorTest: LintTestBase() {
         .files(javaSource("""
           package foo;
           import java.util.Locale;
-          
+
           class Sample {
             fun sample() {
               "hello".toLowerCase(Locale.CANADA);
