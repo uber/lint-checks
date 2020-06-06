e68e577e154753d9a110de0ef308e2215ae3e961
Shaishav Gandhi <shaishav@uber.com>
2020-06-06 13:13:01 -0700

    Add docs for annotations

--- recipes/src/main/java/com/uber/lintchecks/recipes/guardrails/JavaOnly.kt
+++ recipes/src/main/java/com/uber/lintchecks/recipes/guardrails/JavaOnly.kt
@@ -15,4 +15,9 @@
  */
 package com.uber.lintchecks.recipes.guardrails
 
+/**
+ * An annotation that is used to denote methods that should not be called from any
+ * context other than Java code. This is important for cases in APIs that support both Kotlin and
+ * Java in different ways.
+ */
 annotation class JavaOnly
